56694544e7a32164b15a945704bc5eaebc62f87d
Abbasali343 <60543462+Abbasali343@users.noreply.github.com>
2023-11-24 16:41:16 +0500

    made some changes in schema

--- src/Schemas/index.jsx
+++ src/Schemas/index.jsx
@@ -4,16 +4,13 @@ const phoneRegExp =
   /^((\\+[1-9]{1,4}[ \\-]*)|(\\([0-9]{2,3}\\)[ \\-]*)|([0-9]{2,4})[ \\-]*)*?[0-9]{3,4}?[ \\-]*[0-9]{3,4}?$/;
 
 export const adminSchema = Yup.object({
-  title: Yup.string()
-    .min(2, "must have 2 word")
-    .max(15, "max have 15 word")
-    .required("Enter a Title"),
+  title: Yup.string().min(2, "must have 2 word").required("Enter a Title"),
   email: Yup.string().email().required("Enter a Valid Email"),
   phoneNo: Yup.string()
     .required("Phone Number is required")
     .matches(phoneRegExp, "Phone number is not valid")
     .min(10, " must contain 10 words")
-    .max(10, " can contain 10 words"),
+    .max(15, " can contain 15 words"),
   description: Yup.string()
     .min(15, "must contain 15 words")
     .max(200, "max contain 200 words")
@@ -25,10 +22,7 @@ export const adminSchema = Yup.object({
 });
 
 export const introSchema = Yup.object({
-  name: Yup.string()
-    .min(2, "must contain 2 words")
-    .max(15, "max contain 15 words")
-    .required("Enter Name"),
+  name: Yup.string().min(2, "must contain 2 words").required("Enter Name"),
   profession: Yup.string()
     .min(2, "must contain 2 words")
     .max(45, "max contain 45 words")
